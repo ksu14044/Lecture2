99060f2ed9fd8b886e13e4a657a3cb8f2327bb50
ksu14044 <mhm1404@naver.com>
2025-08-16 11:53:50 +0900

    최대 인원수

---
+++ src/main/java/Solutions/최대인원수.java
@@ -0,0 +1,45 @@
+package Solutions;
+
+import java.util.*;
+class 최대인원수 {
+    public int solution(int n, int[][] trans, int[][] bookings){
+        int answer=0;
+        Arrays.sort(bookings, (a, b) -> a[0] - b[0]);
+        LinkedList<Integer> list = new LinkedList<>();
+
+        int[] sum = new int[n + 1];
+        for(int i = 0 ; i < trans.length ; i++){
+            sum[trans[i][0]] += trans[i][2];
+            sum[trans[i][1]] -= trans[i][2];
+        }
+        for(int i = 1 ; i < sum.length ; i++){
+            sum[i] += sum[i - 1];
+        }
+
+        int idx = 0;
+        for(int i = 1 ; i <= n ; i++){
+            while(!list.isEmpty() && list.get(0) == i){
+                answer++;
+                list.pollFirst();
+            }
+            while(idx < bookings.length && bookings[idx][0] == i){
+                list.add(bookings[idx][1]);
+                idx++;
+            }
+            Collections.sort(list);
+            while(list.size() > sum[i]){
+                list.pollLast();
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        최대인원수 T = new 최대인원수();
+        System.out.println(T.solution(5, new int[][]{{1, 4, 2}, {2, 5, 1}}, new int[][]{{1, 2}, {1, 5}, {2, 3}, {2, 4}, {2, 5}, {2, 5}, {3, 5}, {3, 4}}));
+        System.out.println(T.solution(5, new int[][]{{2, 3, 1}, {1, 5, 1}}, new int[][]{{2, 5}, {1, 5}, {1, 3}, {2, 4}, {2, 5}, {2, 3}}));
+        System.out.println(T.solution(8, new int[][]{{1, 8, 3}, {3, 8, 1}}, new int[][]{{1, 3}, {5, 8}, {2, 7}, {3, 8}, {2, 7}, {2, 8}, {3, 8}, {6, 8}, {7, 8}, {5, 8}, {2, 5}, {2, 7}, {3, 7}, {3, 8}}));
+        System.out.println(T.solution(9, new int[][]{{1, 8, 3}, {3, 9, 2}, {1, 5, 3}}, new int[][]{{1, 9}, {5, 8}, {2, 9}, {3, 8}, {2, 9}, {1, 9}, {8, 9}, {3, 9}, {1, 8}, {6, 8}, {7, 8}, {5, 8}, {3, 5}, {3, 7}, {4, 7}, {5, 8}}));
+        System.out.println(T.solution(9, new int[][]{{2, 7, 2}, {3, 9, 2}, {1, 5, 3}}, new int[][]{{1, 9}, {4, 8}, {2, 9}, {5, 9}, {3, 8}, {2, 9}, {1, 9}, {8, 9}, {3, 9}, {1, 8}, {6, 8}, {3, 6}, {7, 8}, {5, 8}, {3, 5}, {2, 7}, {1, 7}, {2, 8}}));
+    }
+}
