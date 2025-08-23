17c2d63a360c63652a765834f18301cc77d73d1f
ksu14044 <mhm1404@naver.com>
2025-08-23 14:58:14 +0900

    계단 오르기

---
+++ src/main/java/Solutions/계단오르기.java
@@ -0,0 +1,28 @@
+package Solutions;
+
+import java.util.Scanner;
+
+public class 계단오르기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] stairs = new int[n + 1];
+        for(int i = 1 ; i <= n ; i++){
+            stairs[i] = sc.nextInt();
+        }
+        if(n <= 1){
+            System.out.println(stairs[1]);
+            return;
+        }
+        int[][] dp = new int[n + 1][2];
+        dp[1][0] = stairs[1];
+        dp[2][0] = stairs[1] + stairs[2];
+        dp[2][1] = stairs[2];
+        for(int i = 3 ; i <= n ; i++){
+            dp[i][0] = dp[i - 1][1] + stairs[i];
+            dp[i][1] = Math.max(dp[i - 2][0], dp[i - 2][1]) + stairs[i];
+        }
+        System.out.println(Math.max(dp[n][0], dp[n][1]));
+        sc.close();
+    }
+}
