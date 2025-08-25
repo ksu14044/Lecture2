0e05568a6c0c3686476da71a657a632e5d01af7c
ksu14044 <mhm1404@naver.com>
2025-08-25 11:43:10 +0900

    수확

--- src/main/java/Solutions/수확.java
+++ src/main/java/Solutions/수확.java
@@ -6,23 +6,25 @@ public class 수확 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] nums = new int[n];
-        for(int i = 0 ; i < n ; i++){
+        int[] nums = new int[n + 1];
+        for(int i = 1 ; i <= n ; i++){
             nums[i] = sc.nextInt();
         }
         sc.close();
-        int[][] dp = new int[n][n];
+        int[][] dp = new int[n  + 1][n + 1];
         for(int i = 0 ; i < n ; i++){
-            dp[i][i] = nums[i] * n;
+            dp[i][i] = nums[i];
         }
-        for(int len = 2 ; len <= n ; len++){
-            for(int l = 0 ; l + len - 1 < n ; l++){
-                int r = l + len - 1;
-                int k = n - (r - l);
-                dp[l][r] = Math.max(nums[l] * k + dp[l + 1][r], nums[r] * k + dp[l][r - 1]);
+        int[] sum = new int[n + 1];
+        sum[1] = nums[1];
+        for(int i = 2 ; i <= n ; i++){
+            sum[i] = sum[i - 1] + nums[i];
+        }
+        for(int i = 1; i <= n ; i++){
+            for(int j = 1; j + i <= n ; j++){
+                dp[j][j + i] = Math.max(dp[j + 1][j + i], dp[j][j + i - 1]) + sum[j + i] - sum[j - 1];
             }
         }
-
-        System.out.println(dp[0][n-1]);
+        System.out.println(dp[1][n]);
     }
 }
